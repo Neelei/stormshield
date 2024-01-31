280e317e50aedffc267b992f710b8a812d219b81
julien.bossuet <julien.bossuet@monacodigital.mc>
2024-01-31 23:29:00 +0100

    first commit hehe

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from stormshield.sns.sslclient import SSLClient
+import pandas as pd
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+client = SSLClient(
+    host="172.16.92.193", port=443,
+    user='admin', password='Toto1234',
+    sslverifyhost=False)
+
+list_peer = client.send_command("CONFIG IPSEC PEER LIST")
+show_peer = client.send_command("CONFIG IPSEC PEER SHOW  name=Site_SITE3-WAN1")
+ipsec_list_gw = client.send_command("CONFIG IPSEC POLICY GATEWAY LIST slot=01")
+list_phase1 = client.send_command("CONFIG IPSEC PROFILE PHASE1 LIST")
+show_phase1 = client.send_command("CONFIG IPSEC PROFILE PHASE1 SHOW name=DR")
+list_phase2 = client.send_command("CONFIG IPSEC PROFILE PHASE2 LIST")
+show_phase2 = client.send_command("CONFIG IPSEC PROFILE PHASE2 PROPOSALS AUTHENTICATION LIST name=DR")
+monitor_filter = client.send_command("MONITOR FILTER")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+monitor_spd = client.send_command("MONITOR GETSPD")
+# print(ipsec_gw_list.data['result'])
+# print(monitor_spd.data['result'][1])
+# print(f'LISTE PEER\n{list_peer}\n')
+# print(f'SHOW PEER Site_SITE3-WAN1\n{show_peer}\n')
+# print(f'LISTE IPSEC GW\n{ipsec_list_gw}\n')
+# print(f'LISTE PHASE1\n{list_phase1}\n')
+# print(f'SHOW PHASE1 DR\n{show_phase1}\n')
+# print(f'LISTE PHASE2\n{list_phase2}\n')
+# print(f'SHOW PHASE2 DR\n{show_phase2}\n')
+print (monitor_filter.data['result'])
+print("toto")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client.disconnect()
--- test.py
+++ test.py
@@ -1,1 +1,42 @@
-print("toto")
+import pandas as pd
+
+# Exemple de réponse JSON de la commande 'MONITOR GETSPD'
+monitor_spd_response = {
+    'rulename': '188e9c85e31_2',
+    'ikerulename': 'Site_SITE3-WAN1',
+    'src': '172.31.1.1',
+    'srcmask': '32',
+    'srcname': 'NAT_MAINCARE_PPROD',
+    'dst': '172.30.1.1',
+    'dstmask': '32',
+    'dstname': 'NAT_CALIMED_PPROD',
+    'srcgw': '172.16.92.193',
+    'srcgwname': 'Firewall_WAN1',
+    'dstgw': '172.16.92.129',
+    'dstgwname': 'SITE3-WAN1',
+    'type': 'gateway',
+    'global': '0',
+    'localid': 'C=MC, ST=MONACO, L=Monaco, O=Monaco Digital, OU=Monaco Digital, CN=CERT-SITE4',
+    'peerid': '',
+    'ike': '2',
+    'enc': 'esp',
+    'policy': 'tunnel',
+    'maxlifetime': '34560'
+}
+
+# Création d'un DataFrame à partir du JSON
+df = pd.DataFrame([{
+    'Référence': monitor_spd_response['rulename'],
+    'IP source Public': monitor_spd_response['src'],
+    'IP destination Public': monitor_spd_response['dst'],
+    'IP INTERFACE': monitor_spd_response['srcgw'],
+    'IP Interface Remote': monitor_spd_response['dstgw'],
+    'Phase 1': monitor_spd_response['localid'],
+    'Phase 2': monitor_spd_response['policy'],
+}])
+
+# Sauvegarde du DataFrame en fichier Excel
+excel_path = 'VPN_IPsec_Data.xlsx'
+df.to_excel(excel_path, index=False)
+
+print("Fichier Excel créé : " + excel_path)
